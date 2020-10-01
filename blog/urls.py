3cdfac6c3ba6fa97f388059e5dcb43beebc74deb
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-09-30 23:25:25 -0300

    Working with Django templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', hello_world),
+    path('blog/', include('website.urls')),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_blog(request):
+    lista = ['Django', 'Python', 'Git', 'html',
+             'Banco de dados', 'linux', 'Nginx', 'Uwsgi',
+             'Systemctl']
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    return render(request, 'indexgeral.html', data)
