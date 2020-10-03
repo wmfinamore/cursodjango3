3da9c617d5ff69859a12d242032bd3855bfef97c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-10-02 22:30:33 -0300

    More about Django admin

--- website/admin.py
+++ website/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title','sub_title','full_desc']
     search_fields = ['title','sub_title']
-    fields = ['title', 'sub_title']
+    # fields = ['title', 'sub_title']
 
 
 admin.site.register(Post, PostAdmin)
--- website/models.py
+++ website/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
 
     def __str__(self):
         return self.title
