9aee01a8b1e7a67b99b5fa481fa290e31140ecab
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-10-03 21:50:51 -0300

    Render the templates and media files

--- website/models.py
+++ website/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
         default=Categorias.GR,
     )
     deleted = models.BooleanField(default=False)
+    imagem = models.ImageField(upload_to='posts', null=True, blank=True)
 
     def __str__(self):
         return self.title
